242bc4c2e5b347b65dd735bebca4fa9415c4b4c7
naye0ng <nayeong_e@naver.com>
2019-08-08 17:59:13 +0900

    2019.08.08 : 구슬탈출2(시간초과)

---
+++ Programmers/그리디/조이스틱.py
@@ -0,0 +1,19 @@
+"""
+조이스틱
+"""
+# def UpDown(an) :
+#     count = 0
+#     for a in range(an) :
+def solution(name):
+    answer = 0
+    name =list(name)
+
+    
+    
+    
+
+
+    return answer
+
+print(solution("JEROEN"))
+# print ord("Z")  print chr(90)
