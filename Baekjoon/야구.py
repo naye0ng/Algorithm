18fd2d08bc65e8b2604c03eb4c266092895b78a0
naye0ng <nayeong_e@naver.com>
2020-03-11 13:55:12 +0900

    2020.03.11 : 야구 - 미해결

--- Baekjoon/야구.py
+++ Baekjoon/야구.py
@@ -1,42 +1,70 @@
 """
 야구
-https://www.acmicpc.net/submit/17281
+https://www.acmicpc.net/problem/17281
 """
-from itertools import permutations
+max_score = 0
+# 득점 계산
+def get_score(player) :
+    """
+    [시간초과] 
+    배열 쓰는거 자체가 사치
+    """
+    b1, b2, b3 = 0, 0, 0 # 1루 2루 3루
+    score = 0
+    idx, inning_idx, out = 0, 0, 0
+    while inning_idx < N :
+        play = inning[inning_idx][player[idx]]
+        if play == 0 :
+            # 아웃
+            out += 1
+            if out == 3 :
+                # 다음 이닝 진행 >> 모두 초기화!
+                inning_idx += 1
+                out, b1, b2, b3 = 0, 0, 0, 0
+        elif play == 1 :
+            # 안타 - 모두 한 루씩 이동
+            score += b3
+            b3, b2, b1 = b2, b1, 1
+        elif play == 2 :
+            # 2루타 - 모두 두 루씩 이동
+            score += b3 + b2
+            b3, b2, b1 = b1, 1, 0
+        elif play == 3 :
+            # 3루타 - 모두 세 루씩 이동 
+            score += b3 + b2 + b1
+            b3, b2, b1 = 1, 0, 0
+        elif play == 4 :
+            # 홈런
+            score += b3 + b2 + b1 +1
+            b1, b2, b3 = 0, 0, 0
+        idx = (idx+1)%9
 
-maxScore = -1
-def makeScore(N, players) :
-    score = n = i = 0 
-    for n in range(N):
-        out = 0
-        f1, f2, f3 = False, False, False
-        while out < 3 :
-            p = players[i]
-            if roundN[n][p] == 1 :
-                score += f3
-                f1, f2, f3 = True, f1, f2
-            elif roundN[n][p] == 2 :
-                score += f2 + f3
-                f1, f2, f3 = False ,True, f1
-            elif roundN[n][p] == 3 :
-                score += f1 + f2 + f3
-                f1, f2, f3 = False, False ,True
-            elif roundN[n][p] == 4 :
-                score += f1 + f2 + f3 + 1
-                f1, f2, f3 = False, False, False
-            elif roundN[n][p] == 0 :
-                out += 1
-            i = (i+1)% 9
-    global maxScore
-    if maxScore < score :
-        maxScore = score                           
+    global max_score 
+    max_score = max(max_score, score)
+
+# 타순 정하기
+def get_player(n, player) :
+    if n == 9 : 
+        get_score(player)
+    elif n == 4 :
+        get_player(n+1, player)
+    else :
+        for i in range(9) :
+            if not visited[i] :
+                visited[i] = True
+                player[n] = i
+                get_player(n+1, player)
+                player[n] = 0
+                visited[i] = False
 
 N = int(input())
-roundN = [ list(map(int, input().split())) for _ in range(N)]
-arr = [ i for i in range(9)]
-for P in permutations(arr[1:]):
-    makeScore(N, P[:3]+(arr[0],)+P[3:])
-print(maxScore)
+inning = [[] for _ in range(N)]
 
+for i in range(N) :
+    inning[i] = list(map(int, input().split()))
 
+visited = [False]*9
+visited[0] = True
+get_player(0, [0]*9) 
 
+print(max_score)
