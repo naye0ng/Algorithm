1a8a26a66c07cacd0a63f2c9a5cdba383c77013e
naye0ng <nayeong_e@naver.com>
2020-05-22 14:25:47 +0900

    2020.05.22 : 방의개수 - 미해결

---
+++ Programmers/그래프/방의개수.py
@@ -0,0 +1,21 @@
+"""
+방의개수
+https://programmers.co.kr/learn/courses/30/lessons/49190?language=python3
+"""
+
+def solution(arrows):
+    answer = 0
+
+
+    return answer
+
+    # 자기 자신이 나오면 연결됐다고 볼 수 있음 >> 그렇다면 중복은 어떻게 해결할 것인가?
+
+
+
+
+
+
+
+
+solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0])
