00bb740331972a61170bcd69a057bd5e22301159
naye0ng <nayeong_e@naver.com>
2020-06-06 13:32:17 +0900

    2020.06.06 : 보호필름 해결

--- SWExpertAcademy/2020삼성A형대비/보호필름.py
+++ SWExpertAcademy/2020삼성A형대비/보호필름.py
@@ -1,64 +1,56 @@
 """
 보호필름
-https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V1SYKAaUDFAWu
+https://swexpertacademy.com/main/solvingProblem/solvingProblem.do
 """
-def is_pass() :
-    for y in range(W):
-        is_not_pass = True
-        before, cnt = -1,0
-        for x in range(D) :
-            if before != cell[x][y] :
-                before = cell[x][y]
-                cnt = 1
-            else :
+def pass_test() :
+    for y in range(W) :
+        is_pass, X, cnt = False, board[0][y], 1
+        for x in range(1, D):
+            if X == board[x][y] :
                 cnt += 1
-                if cnt >= K :
-                    is_not_pass = False
-                    break
-        # 하나라도 부적격이면 통과 못함
-        if is_not_pass :
+            else :
+                X = board[x][y]
+                cnt = 1
+            if cnt >= K :
+                is_pass = True
+                break
+        if not is_pass :
             return False
     return True
 
-# 시약 떨어뜨릴 위치 찾기
-def change_cell(n, k) :
-    global result
-    if is_pass() :
-        result = min(result, n)
-    elif n < result :
-        for m in range(k, D) :
-            tmp = cell[m]
-            cell[m] = [0]*W
-            change_cell(n+1, m+1)
-            cell[m] = [1]*W
-            change_cell(n+1, m+1)
-            cell[m] = tmp
-            
+def drop_reagent(cnt, k) :
+    global drop_count
+    if cnt < drop_count :
+        # [1] 시약 검사
+        if pass_test() :
+            drop_count = cnt
+        # [2] 약품 치기
+        else :
+            for i in range(k, D) :
+                temp = board[i]
+                board[i] = [0]*W
+                drop_reagent(cnt+1, i+1)
+                board[i] = [1]*W
+                drop_reagent(cnt+1, i+1)
+                board[i] = temp
 
 T = int(input())
 for test_case in range(1, 1+T) :
     D, W, K = map(int, input().split())
-    cell = [list(map(int, input().split())) for _ in range(D)]
-    result = K
-    change_cell(0, 0)
-    print('#{} {}'.format(test_case, result))
+    board = [list(map(int, input().split())) for _ in range(D)]
+
+    drop_count = D
+    drop_reagent(0, 0)
+    print('#{} {}'.format(test_case, drop_count))
 
 """
-1
+
+1   
 6 8 3         
 0 0 1 0 1 0 0 1
 0 1 0 0 0 1 1 1
 0 1 1 1 0 0 0 0
 1 1 1 1 0 0 0 1
 0 1 1 0 1 0 0 1
-1 0 1 0 1 1 0 1
-
-1
-6 8 3         
-0 0 1 0 1 0 0 1
-0 1 0 0 0 1 1 1
-0 1 1 1 0 0 0 0
-1 1 1 0 0 0 0 1
-1 1 1 0 1 0 0 1
 1 0 1 0 1 1 0 1
 """
